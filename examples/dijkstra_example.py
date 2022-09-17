12f1ae6b9f9708eabf4bf456c8bae6f6e5ea0a31
Alrefa3ee <89798247+Alrefa3ee@users.noreply.github.com>
2022-09-17 11:22:42 +0300

    some modifications

--- examples/dijkstra_example.py
+++ examples/dijkstra_example.py
@@ -1,5 +1,5 @@
 from ..dijkstra import Dijkstra
-import pprint
+
 
 graph = {'A': {'B': 25, 'C': 20, 'D': 10,"E":105},
          'B': {'A': 20, 'C': 15, 'D': 80,"E":80},
@@ -7,12 +7,31 @@ graph = {'A': {'B': 25, 'C': 20, 'D': 10,"E":105},
          'D': {'A': 10, 'B': 10, 'C': 50, 'E': 100},
          'E': {'A': 40,"B": 50,"C": 5,"D": 10}}
 
-dijkstra = Dijkstra(graph, start_vertex='A')
+def shortest_path(g:dict,start_node,end_node):
 
-# Run the algorithm
-dijkstra.dijkstra()
+        dijkstra = Dijkstra(g, start_vertex=start_node)
 
-pprint.pprint(dijkstra.vertex_labels["E"]["distance"])
+         # Run the algorithm
+        dijkstra.dijkstra()
 
-for vertex in dijkstra.vertices:
-    dijkstra.build_path(vertex)
+         
+        price = dijkstra.vertex_labels[end_node]["distance"]
+        path = ""
+        for vertex in dijkstra.vertices:
+             if vertex == end_node:
+                 path = dijkstra.build_path(vertex)
+             else:
+                 pass
+
+        return price,path
+
+print(shortest_path(graph,"A","D"))
+
+# Output:
+#         price     path
+#        ( 10  , ['A', 'D'])
+
+print(shortest_path(graph,"A","E"))
+# Output:
+#         price       path
+#        ( 100 , ['A', 'D', 'B', 'E'])
