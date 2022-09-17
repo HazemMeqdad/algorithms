c7a14f2dc7e71b7ad5ddf6bedfbfa4dc622e7816
Alrefa3ee <89798247+Alrefa3ee@users.noreply.github.com>
2022-09-17 11:24:08 +0300

    some modifications

--- examples/dijkstra_example.py
+++ examples/dijkstra_example.py
@@ -1,4 +1,4 @@
-from ..dijkstra import Dijkstra
+from ..dijkstra import shortest_path
 
 
 graph = {'A': {'B': 25, 'C': 20, 'D': 10,"E":105},
@@ -6,24 +6,6 @@ graph = {'A': {'B': 25, 'C': 20, 'D': 10,"E":105},
          'C': {'A': 30, 'B': 15,"D":70,"E":90},
          'D': {'A': 10, 'B': 10, 'C': 50, 'E': 100},
          'E': {'A': 40,"B": 50,"C": 5,"D": 10}}
-
-def shortest_path(g:dict,start_node,end_node):
-
-        dijkstra = Dijkstra(g, start_vertex=start_node)
-
-         # Run the algorithm
-        dijkstra.dijkstra()
-
-         
-        price = dijkstra.vertex_labels[end_node]["distance"]
-        path = ""
-        for vertex in dijkstra.vertices:
-             if vertex == end_node:
-                 path = dijkstra.build_path(vertex)
-             else:
-                 pass
-
-        return price,path
 
 print(shortest_path(graph,"A","D"))
 
